63e7a81bdd8a56e338f9263d85ecbb08ce4021b1
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2025-05-14 01:31:51 +0530

    aashuu

--- components/ui/GradientBg.tsx
+++ components/ui/GradientBg.tsx
@@ -39,7 +39,8 @@ export const BackgroundGradientAnimation = ({
   const [curY, setCurY] = useState(0);
   const [tgX, setTgX] = useState(0);
   const [tgY, setTgY] = useState(0);
-  useEffect(() => {
+useEffect(() => {
+  if (typeof window !== "undefined") {
     document.body.style.setProperty(
       "--gradient-background-start",
       gradientBackgroundStart
@@ -56,7 +57,8 @@ export const BackgroundGradientAnimation = ({
     document.body.style.setProperty("--pointer-color", pointerColor);
     document.body.style.setProperty("--size", size);
     document.body.style.setProperty("--blending-value", blendingValue);
-  }, []);
+  }
+}, []);
 
   useEffect(() => {
     function move() {
